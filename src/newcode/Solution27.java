db7403d5cc2c82cb4b701588ff76a336c7b4acd3
dnhua <suehuachaoli@gmail.com>
2019-03-15 19:41:35 +0800

    链表的镜像问题

--- src/newcode/Solution27.java
+++ src/newcode/Solution27.java
@@ -2,7 +2,44 @@ package newcode;
 
 /**
  * 二叉树的镜像
- *
+ *操作给定的二叉树，将其变换为源二叉树的镜像。
+ * 输入描述:
+ * 二叉树的镜像定义：源二叉树
+ *     	     8
+ *     	   /  \
+ *     	  6   10
+ *     	 / \  / \
+ *     	5  7 9 11
+ *     	镜像二叉树
+ *     	     8
+ *     	   /  \
+ *     	  10   6
+ *     	 / \  / \
+ *     	11 9 7  5
  */
 public class Solution27 {
+    public void Mirror(TreeNode root) {
+        if (root == null)
+            return;
+        Mirror(root.left);
+        Mirror(root.right);
+        swap(root, root.left, root.right);
+
+    }
+
+    private void swap(TreeNode root, TreeNode left, TreeNode right) {
+        if (root == null)
+            return;
+        TreeNode tem = left;
+        root.left = right;
+        root.right = tem;
+    }
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
 }
---
+++ src/newcode/Solution28.java
@@ -0,0 +1,28 @@
+package newcode;
+
+public class Solution28 {
+    boolean isSymmetrical(TreeNode pRoot) {
+        if (pRoot == null)
+            return true;
+        return isSymmetrical(pRoot.left, pRoot.right);
+    }
+
+    boolean isSymmetrical(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null)
+            return true;
+        if (t1 == null || t2 == null)
+            return false;
+        if (t1.val != t2.val)
+            return false;
+        return isSymmetrical(t1.left, t2.right) && isSymmetrical(t1.right, t2.left);
+    }
+
+    private class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}
