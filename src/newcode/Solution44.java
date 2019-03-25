d89461e8aee4742cae45bb7b16a7416610cf55a0
dnhua <suehuachaoli@gmail.com>
2019-03-25 19:51:31 +0800

    连续子数组的最大和

---
+++ src/newcode/Solution44.java
@@ -0,0 +1,24 @@
+package newcode;
+
+/**
+ * 连续子数组的最大和
+ * HZ偶尔会拿些专业问题来忽悠那些非计算机专业的同学。今天测试组开完会
+ * 后,他又发话了:在古老的一维模式识别中,常常需要计算连续子向量的最大
+ * 和,当向量全为正数的时候,问题很好解决。但是,如果向量中包含负数,是否
+ * 应该包含某个负数,并期望旁边的正数会弥补它呢？例如:{6,-3,-2,7,-15,
+ * 1,2,2},连续子向量的最大和为8(从第0个开始,到第3个为止)。给一个数组，
+ * 返回它的最大连续子序列的和，你会不会被他忽悠住？(子向量的长度至少是1)
+ */
+public class Solution44 {
+    public int FindGreatestSumOfSubArray(int[] array) {
+        if (array == null || array.length == 0)
+            return 0;
+        int maxsum = array[0];
+        int sum = 0;
+        for (int num : array) {
+            sum = sum <= 0 ? num : sum + num;
+            maxsum = Math.max(sum, maxsum);
+        }
+        return maxsum;
+    }
+}
