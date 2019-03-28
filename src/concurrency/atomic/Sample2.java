65aa9419982a8f2f5dd22d0cd384f6bf85454a02
dnhua <suehuachaoli@gmail.com>
2019-03-28 15:43:06 +0800

    线程顺序执行,艺龙笔试题

---
+++ src/concurrency/atomic/Sample2.java
@@ -0,0 +1,67 @@
+package concurrency.atomic;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 实现线程顺序执行方式1：
+ * 这是我做的艺龙的一道笔试题
+ * 输入 hello
+ * 线程1，2，3分别给这个字符串拼接 _a,_b,_c
+ * 最后打印 hello_a_b_c
+ */
+public class Sample2 {
+    private String s = "hello";
+    @Test
+    public void test() {
+        final Alternate alternate = new Alternate();
+        final Thread a = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                alternate.addA();
+            }
+        },"A");
+        a.start();
+        final Thread b = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    a.join();
+                    alternate.addB();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        },"B");
+        b.start();
+        Thread c = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    b.join();
+                    alternate.addC();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        },"C");
+        c.start();
+        try {
+            c.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(s);
+    }
+
+    class Alternate{
+        public void addA(){
+            s += "_a";
+        }
+        public void addB(){
+            s += "_b";
+        }
+        public void addC(){
+            s += "_c";
+        }
+    }
+}
