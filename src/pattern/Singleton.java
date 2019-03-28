960d7e6c58084e5064fddbf288aedebf413430bc
dnhua <suehuachaoli@gmail.com>
2019-03-28 14:50:47 +0800

    单例模式

--- src/pattern/Singleton.java
+++ src/pattern/Singleton.java
@@ -5,10 +5,10 @@ package pattern;
  */
 public class Singleton {
     private static volatile Singleton instance;
+    //私有的构造方法保证不能直接new一个对象出来
     private Singleton(){
 
     }
-
     public static Singleton getInstance() {
         synchronized (Singleton.class) {
             if (instance == null) {
