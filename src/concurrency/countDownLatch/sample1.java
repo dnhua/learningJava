b122e9c1388c9a2faf67e66ebcef81d327a9c9fb
dnhua <suehuachaoli@gmail.com>
2019-03-31 13:40:13 +0800

    给定一个任务A，它要等待其他4个任务执行完毕之后才能执行

---
+++ src/concurrency/countDownLatch/sample1.java
@@ -0,0 +1,49 @@
+package concurrency.countDownLatch;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * CountDownLatch类位于java.util.concurrent包下，利用它可以实现类似计数器的功能。
+ * 比如有一个任务A，它要等待其他4个任务执行完毕之后才能执行，此时就可以利用
+ * CountDownLatch来实现这种功能了。
+ */
+public class sample1 {
+    public static void main(String[] args) {
+        final CountDownLatch latch = new CountDownLatch(2);
+
+        new Thread(){
+            public void run() {
+                try {
+                    System.out.println("子线程"+Thread.currentThread().getName()+"正在执行");
+                    Thread.sleep(3000);
+                    System.out.println("子线程"+Thread.currentThread().getName()+"执行完毕");
+                    latch.countDown();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            };
+        }.start();
+
+        new Thread(){
+            public void run() {
+                try {
+                    System.out.println("子线程"+Thread.currentThread().getName()+"正在执行");
+                    Thread.sleep(3000);
+                    System.out.println("子线程"+Thread.currentThread().getName()+"执行完毕");
+                    latch.countDown();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            };
+        }.start();
+
+        try {
+            System.out.println("等待2个子线程执行完毕...");
+            latch.await();
+            System.out.println("2个子线程已经执行完毕");
+            System.out.println("继续执行主线程");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
