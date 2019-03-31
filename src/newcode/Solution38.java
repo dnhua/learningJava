681889242fb309beb460f63750c80c6a16196f78
dnhua <huazailixtu@outlook.com>
2019-03-31 15:05:03 +0800

    38

---
+++ src/newcode/Solution38.java
@@ -0,0 +1,25 @@
+package newcode;
+
+import java.util.Queue;
+import java.util.Stack;
+
+/**
+ * 输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的
+ * 双向链表。要求不能创建任何新的结点，只能调整树中结
+ * 点指针的指向。
+ */
+public class Solution38 {
+    public TreeNode Convert(TreeNode pRootOfTree) {
+
+        return  null;
+    }
+
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}
