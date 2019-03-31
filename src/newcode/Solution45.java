95fa966514ac2b245bee73a6ea29c432d8e39326
dnhua <huazailixtu@outlook.com>
2019-03-31 16:09:17 +0800

    二进制1的总数

---
+++ src/newcode/Solution45.java
@@ -0,0 +1,32 @@
+package newcode;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 二进制1的总数
+ */
+public class Solution45 {
+    public int NumberOf1Between1AndN_Solution(int n) {
+        int res = 0;
+        for (int i=1; i<=n; i++) {
+            res += NumberOfN(i);
+        }
+        return res;
+    }
+
+    public int NumberOfN(int n) {
+        int cnt = 0;
+        while (n != 0) {
+            n = n & (n-1);
+            cnt++;
+        }
+        return cnt;
+    }
+
+    @Test
+    public void test1() {
+        Solution45 solution45 = new Solution45();
+        int number = solution45.NumberOf1Between1AndN_Solution(5);
+        System.out.println(number);
+    }
+}
