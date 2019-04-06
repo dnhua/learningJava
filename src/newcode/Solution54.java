3adc3941f22d4b61514669b7395af12891f15b42
dnhua <huazailixtu@outlook.com>
2019-04-06 22:10:50 +0800

    两个链表的第一个公共结点

---
+++ src/newcode/Solution54.java
@@ -0,0 +1,26 @@
+package newcode;
+
+/**
+ * 两个链表的第一个公共结点
+ */
+public class Solution54 {
+    public ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
+        ListNode l1 = pHead1, l2 = pHead2;
+        while (l1 != null || l2 != null) {
+            if (l1 == l2)
+                break;
+            l1 = (l1 == null) ? pHead2 : l1.next;
+            l2 = (l2 == null) ? pHead1 : l2.next;
+        }
+        return l1;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+}
