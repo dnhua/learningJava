3265449a2c36d66d7c2d1fc422895a4a29fbf3e7
dnhua <huazailixtu@outlook.com>
2019-04-08 09:40:11 +0800

    简单工厂模式

--- src/pattern/factory/simpleFactory/SimpleNoodlesFactory.java
+++ src/pattern/factory/simpleFactory/SimpleNoodlesFactory.java
@@ -1,9 +1,9 @@
 package pattern.factory.simpleFactory;
 
 public class SimpleNoodlesFactory {
-    public static final int TYPE_Rice = 1;//兰州拉面
-    public static final int TYPE_Sliced = 2;//泡面
-    public static final int TYPE_Instant = 3;//干扣面
+    public static final int TYPE_Rice = 1;
+    public static final int TYPE_Sliced = 2;
+    public static final int TYPE_Instant = 3;
 
     public static INoodles createNoodles(int type) {
         switch (type) {
