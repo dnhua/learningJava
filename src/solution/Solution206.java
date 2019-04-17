1b96a486eb9e6b25e88e8373e9e0691b6051e3ab
dnhua <huazailixtu@outlook.com>
2019-04-17 15:45:31 +0800

    链表反转

---
+++ src/solution/Solution206.java
@@ -0,0 +1,39 @@
+package solution;
+
+/**
+ * 链表反转
+ */
+public class Solution206 {
+    /**
+     * 头插法
+     * @param head
+     * @return
+     */
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = new ListNode(-1);   //使用prev.next表示前驱节点
+        ListNode next = null;
+        while (head != null) {
+            next = head.next;   //保存next节点
+            head.next = prev.next;  //让head的下一个节点连接到head.next
+            prev.next = head;   //更新prev.next
+            head = next;    //更新head
+        }
+        return prev.next;
+    }
+
+    /**
+     * 递归法
+     * @param head
+     * @return
+     */
+    public ListNode reverseList2(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode next = head.next;
+        ListNode newHead = reverseList(next);
+        next.next = head;
+        head.next = null;
+        return newHead;
+    }
+}
