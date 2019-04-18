b1464beb7eaec6f649f4429d34ebf9db664e3bd9
dnhua <huazailixtu@outlook.com>
2019-04-18 16:26:15 +0800

    链表求和

---
+++ src/solution/Solution445.java
@@ -0,0 +1,36 @@
+package solution;
+
+import java.util.Stack;
+
+/**
+ * 链表求和
+ * Input: (7 -> 2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 8 -> 0 -> 7
+ */
+public class Solution445 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Stack<Integer> l1Stack = buildStack(l1);
+        Stack<Integer> l2Stack = buildStack(l2);
+        ListNode head = new ListNode(-1);
+        int carry = 0;
+        while (!l1Stack.isEmpty() || !l2Stack.isEmpty() || carry != 0) {
+            int x = l1Stack.isEmpty() ? 0 : l1Stack.pop();
+            int y = l2Stack.isEmpty() ? 0 : l2Stack.pop();
+            int sum = x + y + carry;
+            ListNode node = new ListNode(sum % 10);
+            node.next = head.next;
+            head.next = node;
+            carry = sum / 10;
+        }
+        return head.next;
+    }
+
+    private Stack<Integer> buildStack(ListNode l) {
+        Stack<Integer> stack = new Stack<>();
+        while (l != null) {
+            stack.push(l.val);
+            l = l.next;
+        }
+        return stack;
+    }
+}
