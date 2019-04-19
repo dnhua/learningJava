4b57d4769c2863e633ae75947ec6a1bcc9a32417
dnhua <huazailixtu@outlook.com>
2019-04-19 16:16:39 +0800

    树的高度

---
+++ src/solution/tree/Solution104.java
@@ -0,0 +1,12 @@
+package solution.tree;
+
+/**
+ * 树的高度
+ */
+public class Solution104 {
+    public int maxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
