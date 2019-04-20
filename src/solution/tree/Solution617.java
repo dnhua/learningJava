c1ddd005947208c25f4750443587ec4aa5dde3e1
dnhua <huazailixtu@outlook.com>
2019-04-20 15:15:24 +0800

    归并两棵树

---
+++ src/solution/tree/Solution617.java
@@ -0,0 +1,16 @@
+package solution.tree;
+
+/**
+ * 归并两棵树
+ */
+public class Solution617 {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) return null;
+        if (t1 == null) return t2;
+        if (t2 == null) return t1;
+        TreeNode root = new TreeNode(t1.val + t2.val);
+        root.left = mergeTrees(t1.left, t2.left);
+        root.right = mergeTrees(t1.right, t2.right);
+        return root;
+    }
+}
