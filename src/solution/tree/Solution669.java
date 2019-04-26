e9583234766f6b4d0bc2701638f60ccd051b6f7d
dnhua <huazailixtu@outlook.com>
2019-04-26 14:30:07 +0800

    修剪二叉查找树

---
+++ src/solution/tree/Solution669.java
@@ -0,0 +1,15 @@
+package solution.tree;
+
+/**
+ * 修剪二叉查找树
+ */
+public class Solution669 {
+    public TreeNode trimBST(TreeNode root, int L, int R) {
+        if (root == null) return null;
+        if (root.val > R) return trimBST(root.left, L, R);
+        if (root.val < L) return trimBST(root.right, L, R);
+        root.left = trimBST(root.left, L, R);
+        root.right = trimBST(root.right, L, R);
+        return root;
+    }
+}
