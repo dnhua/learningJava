fd08fa031e523213753eed584d639fafebd9dc30
dnhua <huazailixtu@outlook.com>
2019-04-30 15:47:25 +0800

    编程之美 3.1

--- src/solution/string/Solution3_1.java
+++ src/solution/string/Solution3_1.java
@@ -13,11 +13,13 @@ public class Solution3_1 {
     public static void main(String[] args) {
         String s1 = StdIn.readLine();
         String s2 = StdIn.readLine();
+        System.out.println("s1:"+s1);
+        System.out.println("s2:"+s2);
         System.out.println(subString(s1, s2));
     }
 
     public static boolean subString(String s1, String s2) {
-        s1 += s2;
+        s1 += s1;
         return s1.contains(s2);
     }
 }
