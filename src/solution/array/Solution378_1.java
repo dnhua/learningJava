d2b3042281c66f140772437bb5467f764545e609
dnhua <huazailixtu@outlook.com>
2019-05-05 15:08:23 +0800

    二分查找解法

---
+++ src/solution/array/Solution378_1.java
@@ -0,0 +1,23 @@
+package solution.array;
+
+/**
+ * 二分查找解法：
+ */
+public class Solution378_1 {
+    public int kthSmallest(int[][] matrix, int k) {
+        int m = matrix.length, n = matrix[0].length;
+        int lo = matrix[0][0], hi = matrix[m - 1][n - 1];
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            int cnt = 0;
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n && matrix[i][j] <= mid; j++) {
+                    cnt++;
+                }
+            }
+            if (cnt < k) lo = mid + 1;
+            else hi = mid - 1;
+        }
+        return lo;
+    }
+}
