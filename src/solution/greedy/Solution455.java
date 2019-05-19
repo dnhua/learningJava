5dc4f5377a5187e3cb1c8afdcb823ba3d51b32a0
dnhua <huazailixtu@outlook.com>
2019-05-19 14:40:07 +0800

    分配饼干

--- src/solution/greedy/Solution455.java
+++ src/solution/greedy/Solution455.java
@@ -5,10 +5,8 @@ import java.util.Arrays;
 /**
  * 分配饼干
  * 455. Assign Cookies (Easy)
- *
  * Input: [1,2], [1,2,3]
  * Output: 2
- *
  * Explanation: You have 2 children and 3 cookies. The greed factors of 2 children are 1, 2.
  * You have 3 cookies and their sizes are big enough to gratify all of the children,
  * You need to output 2.
