c28d617274bdb3df2804ae5050ecf1fd6005575d
dnhua <huazailixtu@outlook.com>
2019-05-20 21:12:04 +0800

    求开方

---
+++ src/solution/binarySearch/Solution69.java
@@ -0,0 +1,34 @@
+package solution.binarySearch;
+
+/**
+ * 求开方
+ * 69. Sqrt(x) (Easy)
+ *
+ * Input: 4
+ * Output: 2
+ *
+ * Input: 8
+ * Output: 2
+ * Explanation: The square root of 8 is 2.82842..., and since we want to return an integer,
+ * the decimal part will be truncated.
+ */
+public class Solution69 {
+    public int mySqrt(int x) {
+        if (x <= 1) {
+            return x;
+        }
+        int l = 1, h = x;
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+            int sqrt = x / mid;
+            if (sqrt == mid) {
+                return mid;
+            } else if (mid > sqrt) {
+                h = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return h;
+    }
+}
