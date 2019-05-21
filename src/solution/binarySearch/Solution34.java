bb539247c1c711480ca41e1a41c2a227778c84d6
dnhua <huazailixtu@outlook.com>
2019-05-21 15:06:55 +0800

    查找区间

---
+++ src/solution/binarySearch/Solution34.java
@@ -0,0 +1,36 @@
+package solution.binarySearch;
+
+/**
+ * 查找区间
+ * 34. Find First and Last Position of Element in Sorted Array
+ *
+ * Input: nums = [5,7,7,8,8,10], target = 8
+ * Output: [3,4]
+ *
+ * Input: nums = [5,7,7,8,8,10], target = 6
+ * Output: [-1,-1]
+ */
+public class Solution34 {
+    public int[] searchRange(int[] nums, int target) {
+        int first = binarySearch(nums, target);
+        int last = binarySearch(nums, target + 1) - 1;
+        if (first == nums.length || nums[first] != target) {
+            return new int[]{-1, -1};
+        } else {
+            return new int[]{first, Math.max(first, last)};
+        }
+    }
+
+    private int binarySearch(int[] nums, int target) {
+        int l = 0, h = nums.length; // 注意 h 的初始值
+        while (l < h) {
+            int m = l + (h - l) / 2;
+            if (nums[m] >= target) {
+                h = m;
+            } else {
+                l = m + 1;
+            }
+        }
+        return l;
+    }
+}
