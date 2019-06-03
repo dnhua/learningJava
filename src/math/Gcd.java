8715eea7a3a672b651634810eccf2a0bcce787c2
dnhua <huazailixtu@outlook.com>
2019-06-03 16:11:01 +0800

    最大公约数

--- src/math/Gcd.java
+++ src/math/Gcd.java
@@ -10,6 +10,25 @@ public class Gcd {
         return b == 0 ? a : gcd(b, a % b);
     }
 
+    public int gcd2(int a, int b) {
+        if (a < b) {
+            return gcd(b, a);
+        }
+        if (b == 0) {
+            return a;
+        }
+        boolean isAEven = isEven(a), isBEven = isEven(b);
+        if (isAEven && isBEven) {
+            return 2 * gcd(a >> 1, b >> 1);
+        } else if (isAEven && !isBEven) {
+            return gcd(a >> 1, b);
+        } else if (!isAEven && isBEven) {
+            return gcd(a, b >> 1);
+        } else {
+            return gcd(b, a - b);
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int i = sc.nextInt();
