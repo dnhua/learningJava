7143b96d9e7fc1a6f54a46b4a7e664cd20873a40
dnhua <huazailixtu@outlook.com>
2019-06-03 16:08:44 +0800

    最大公约数

--- src/math/Gcd.java
+++ src/math/Gcd.java
@@ -1,10 +1,20 @@
 package math;
 
+import java.util.Scanner;
+
 /**
  * 最大公约数
  */
 public class Gcd {
     int gcd(int a, int b) {
         return b == 0 ? a : gcd(b, a % b);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        int j = sc.nextInt();
+        Gcd g = new Gcd();
+        System.out.println(g.gcd(i,j));
     }
 }
