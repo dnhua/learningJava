d7a5588db885ba411d2ebe70ffcee2e42ed07ece
dnhua <huazailixtu@outlook.com>
2019-06-09 17:24:23 +0800

    划分数组为和相等的两部分

---
+++ src/solution/dp/Solution416.java
@@ -0,0 +1,30 @@
+package solution.dp;
+
+/**
+ * 划分数组为和相等的两部分
+ */
+public class Solution416 {
+    public boolean canPartition(int[] nums) {
+        int sum = computeArraySum(nums);
+        if (sum % 2 != 0) {
+            return false;
+        }
+        int W = sum / 2;
+        boolean[] dp = new boolean[W + 1];
+        dp[0] = true;
+        for (int num : nums) {                 // 0-1 背包一个物品只能用一次
+            for (int i = W; i >= num; i--) {   // 从后往前，先计算 dp[i] 再计算 dp[i-num]
+                dp[i] = dp[i] || dp[i - num];
+            }
+        }
+        return dp[W];
+    }
+
+    private int computeArraySum(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        return sum;
+    }
+}
