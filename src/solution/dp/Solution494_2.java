0ad82b5aa4e1b049a5d63df5d5b7a5db14d95419
dnhua <huazailixtu@outlook.com>
2019-06-14 21:10:59 +0800

    改变一组数的正负号使得它们的和为一给定数2

---
+++ src/solution/dp/Solution494_2.java
@@ -0,0 +1,26 @@
+package solution.dp;
+
+public class Solution494_2 {
+    public int findTargetSumWays(int[] nums, int S) {
+        int sum = computeArraySum(nums);
+        int W = (S + sum) / 2;
+        int[] dp = new int[W+1];
+        dp[0] = 1;
+
+        for (int num : nums) {
+            for (int i=W; i>=num; i--) {
+                dp[i] = dp[i]+dp[i-num];
+            }
+        }
+
+        return dp[W];
+    }
+
+    private int computeArraySum(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        return sum;
+    }
+}
