01af4d78b23e912c3974334d1ba34619118bd88d
dnhua <huazailixtu@outlook.com>
2019-06-16 21:19:53 +0800

    找零钱的硬币数组合

---
+++ src/solution/dp/Solution518.java
@@ -0,0 +1,27 @@
+package solution.dp;
+
+/**
+ * 找零钱的硬币数组合
+ * Input: amount = 5, coins = [1, 2, 5]
+ * Output: 4
+ * Explanation: there are four ways to make up the amount:
+ * 5=5
+ * 5=2+2+1
+ * 5=2+1+1+1
+ * 5=1+1+1+1+1
+ */
+public class Solution518 {
+    public int change(int amount, int[] coins) {
+        if (amount == 0 || coins == null || coins.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+        return dp[amount];
+    }
+}
