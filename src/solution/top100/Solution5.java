4755bcfafa76640a82d0fad2b763e9fff5e30eab
dnhua <huazailixtu@outlook.com>
2019-06-23 15:27:50 +0800

    5. Longest Palindromic Substring

---
+++ src/solution/top100/Solution5.java
@@ -0,0 +1,38 @@
+package solution.top100;
+
+/**
+ * 5. Longest Palindromic Substring
+ * https://leetcode.com/problems/longest-palindromic-substring/
+ */
+public class Solution5 {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expand(s, i, i);
+            int len2 = expand(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expand(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+
+
+    public static void main(String[] args) {
+        String str = "ababc";
+        Solution5 solution5 = new Solution5();
+        solution5.longestPalindrome(str);
+    }
+}
