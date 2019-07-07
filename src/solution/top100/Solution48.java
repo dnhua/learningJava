b1de0c224b5322a5ebaf74efadcccc16b6a53cce
dnhua <huazailixtu@outlook.com>
2019-07-07 20:15:11 +0800

    48. Rotate Image

---
+++ src/solution/top100/Solution48.java
@@ -0,0 +1,69 @@
+package solution.top100;
+
+/**
+ * 48. Rotate Image
+ * You are given an n x n 2D matrix representing an image.
+ *
+ * Rotate the image by 90 degrees (clockwise).
+ *
+ * Note:
+ *
+ * You have to rotate the image in-place, which means you have to modify the input 2D matrix directly.
+ * DO NOT allocate another 2D matrix and do the rotation.
+ *
+ * Example 1:
+ *
+ * Given input matrix =
+ * [
+ *   [1,2,3],
+ *   [4,5,6],
+ *   [7,8,9]
+ * ],
+ *
+ * rotate the input matrix in-place such that it becomes:
+ * [
+ *   [7,4,1],
+ *   [8,5,2],
+ *   [9,6,3]
+ * ]
+ *
+ * Example 2:
+ *
+ * Given input matrix =
+ * [
+ *   [ 5, 1, 9,11],
+ *   [ 2, 4, 8,10],
+ *   [13, 3, 6, 7],
+ *   [15,14,12,16]
+ * ],
+ *
+ * rotate the input matrix in-place such that it becomes:
+ * [
+ *   [15,13, 2, 5],
+ *   [14, 3, 4, 1],
+ *   [12, 6, 8, 9],
+ *   [16, 7,10,11]
+ * ]
+ */
+public class Solution48 {
+    public void rotate(int[][] matrix) {
+        //思路：先沿着对角线翻转，再沿着水平线翻转一次
+        int n = matrix.length;
+        //沿着对角线翻转
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n-i; j++) {
+                int tem = matrix[i][j];
+                matrix[i][j] = matrix[n-j-1][n-i-1];
+                matrix[n-j-1][n-i-1] = tem;
+            }
+        }
+        //沿着水平线翻转
+        for (int i=0; i<n/2; i++) {
+            for (int j=0; j<n; j++) {
+                int tem = matrix[i][j];
+                matrix[i][j] = matrix[n-i-1][j];
+                matrix[n-i-1][j] = tem;
+            }
+        }
+    }
+}
