8c11e7307555baca43ab6708bd499e0a48ed989e
dnhua <huazailixtu@outlook.com>
2019-07-19 20:21:34 +0800

    m和n的顺序反了

--- src/solution/dp/背包问题/Solution2.java
+++ src/solution/dp/背包问题/Solution2.java
@@ -9,10 +9,10 @@ public class Solution2 {
             return 0;
         int m = weights.length, n = Weight;
         int[] dp = new int[n+1];
-        for (int i=1; i<=n; i++) {
-            int w = weights[i];
-            for (int j=m; j>w; j--) {
-                dp[j] = Math.max(dp[j-1], dp[j-w] + values[i]);
+        for (int i=1; i<=m; i++) {
+            int w = weights[i-1];
+            for (int j=n; j>w; j--) {
+                dp[j] = Math.max(dp[j-1], dp[j-w] + values[i-1]);
             }
         }
         return dp[n];
@@ -22,7 +22,7 @@ public class Solution2 {
         int Weight = 1000;
         int[] weights = new int[]{200, 600, 100, 180, 300, 450};
         int[] value = new int[]{6, 10, 3, 4, 5, 8};
-        Solution1 solution1 = new Solution1();
-        System.out.println(solution1.solution(Weight, weights, value));
+        Solution2 solution2 = new Solution2();
+        System.out.println(solution2.solution(Weight, weights, value));
     }
 }
