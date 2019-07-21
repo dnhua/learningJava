cc7e0b0b94bcfbd4936a3df307b816d17f20922b
dnhua <huazailixtu@outlook.com>
2019-07-21 16:49:23 +0800

    二叉树的深度

---
+++ src/solution/牛客部分题二刷/二叉树的深度.java
@@ -0,0 +1,19 @@
+package solution.牛客部分题二刷;
+
+public class 二叉树的深度 {
+    public int TreeDepth(TreeNode root) {
+        return root == null ? 0 : 1 + Math.max(TreeDepth(root.left), TreeDepth(root.right));
+    }
+
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+
+    }
+}
