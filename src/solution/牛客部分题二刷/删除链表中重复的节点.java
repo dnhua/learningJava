25a08782837d98c2469bb461c8e82083e93ba6c3
dnhua <huazailixtu@outlook.com>
2019-07-23 15:21:29 +0800

    删除链表中重复的节点

---
+++ src/solution/牛客部分题二刷/删除链表中重复的节点.java
@@ -0,0 +1,26 @@
+package solution.牛客部分题二刷;
+
+public class 删除链表中重复的节点 {
+    public ListNode deleteDuplication(ListNode pHead){
+        if (pHead == null || pHead.next == null)
+            return pHead;
+        ListNode next = pHead.next;
+        if (pHead.val == next.val) {
+            while (next != null && pHead.val == next.val)
+                next = next.next;
+            return deleteDuplication(next);
+        } else {
+            pHead.next = deleteDuplication(pHead.next);
+            return pHead;
+        }
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+}
