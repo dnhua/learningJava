831f5441aef5cd678a11d6e02d2c7ca26d05c5d8
dnhua <huazailixtu@outlook.com>
2019-08-06 14:40:25 +0800

    大于给定元素的最小元素

---
+++ src/solution/部分leetcode二刷/排序字符数组找target.java
@@ -0,0 +1,18 @@
+package solution.部分leetcode二刷;
+
+class 排序字符数组找target {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int n = letters.length;
+        int l = 0, h = n - 1;
+        while (l <= h) {
+            int m = l + (h - l) / 2;
+            char mchar = letters[m];
+            if (mchar <= target) {
+                l = m + 1;
+            } else {
+                h = m - 1;
+            }
+        }
+        return l < n ? letters[l] : letters[0];
+    }
+}
