d1955ecd3f6f5b2c853f9fdf6afa828421a14ff6
dnhua <huazailixtu@outlook.com>
2019-08-08 15:14:24 +0800

    Kth Largest Element in an Array

---
+++ src/solution/部分leetcode二刷/kth大值.java
@@ -0,0 +1,40 @@
+package solution.部分leetcode二刷;
+
+public class kth大值 {
+    //这题用快排来做
+    public int findKthLargest(int[] nums, int k) {
+        k = nums.length - k;
+        int l = 0, h = nums.length - 1;
+        while (l < h) {
+            int j = partition(nums, l, h);
+            if (j == k) {
+                break;
+            } else if (j < k) {
+                l = j + 1;
+            } else {
+                h = j - 1;
+            }
+        }
+        return nums[k];
+    }
+
+    //partition
+    private int partition(int[] nums, int l, int h) {
+        int i = l, j = h + 1;
+        while (true) {
+            while (nums[++i] < nums[l] && i < h) ;
+            while (nums[--j] > nums[l] && j > l) ;
+            if (i >= j)
+                break;
+            swap(nums, i, j);
+        }
+        swap(nums, l, j);
+        return j;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tem = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tem;
+    }
+}
