1e233f32762eafc982bed5a4bc7bc346ebc3e5ed
dnhua <huazailixtu@outlook.com>
2019-08-08 15:48:16 +0800

    kth大值

--- src/solution/部分leetcode二刷/kth大值.java
+++ src/solution/部分leetcode二刷/kth大值.java
@@ -37,4 +37,10 @@ public class kth大值 {
         nums[i] = nums[j];
         nums[j] = tem;
     }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{3,2,3,1,2,4,5,5,6,4};
+        kth大值 solution = new kth大值();
+        System.out.println(solution.findKthLargest(nums, 4));
+    }
 }
